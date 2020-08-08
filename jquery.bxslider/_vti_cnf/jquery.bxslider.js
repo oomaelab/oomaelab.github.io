62c00bcf1c123dcfce054b685799d2aa21417b7f
oomaelab <69372148+oomaelab@users.noreply.github.com>
2020-08-08 09:48:52 +0900

    Add files via upload

---
+++ jquery.bxslider/_vti_cnf/jquery.bxslider.js
@@ -0,0 +1,9 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|31 Dec 2014 06:21:10 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_author:SR|OOMAE-PC1\\大前
+vti_modifiedby:SR|OOMAE-PC1\\大前
+vti_timecreated:TR|31 Dec 2014 06:21:10 -0000
+vti_cacheddtm:TX|31 Dec 2014 06:21:10 -0000
+vti_filesize:IR|50573
+vti_backlinkinfo:VX|
---
+++ jquery.bxslider/_vti_cnf/readme.md
@@ -0,0 +1,6 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|31 Dec 2014 06:21:10 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_cacheddtm:TX|31 Dec 2014 06:21:10 -0000
+vti_filesize:IR|14741
+vti_backlinkinfo:VX|
