aa08a8a3d733afbd1d3147b99c72b54dbef5f86d
oomaelab <69372148+oomaelab@users.noreply.github.com>
2020-08-09 21:14:04 +0900

    Update navi.js

--- js/navi.js
+++ js/navi.js
@@ -15,10 +15,8 @@ function navigator()
   html += "</p>";
   html += "<p>";
   html += "<button class='btn btn-1 btn-1a' onclick='location.href=\"https://oomaelab.github.io/member.html\"'>メンバー</button>";
-  html += "<button class='btn btn-1 btn-1a' onclick='location.href=\"https://oomaelab.github.io/book.html\"'>研究で参考になる本</button>";
-  html += "</p>";
-  html += "<p>";
   html += "<button class='btn btn-1 btn-1a' onclick='location.href=\"https://oomaelab.github.io/oomaelab/index.html\"'>研究室内部ページ</button>";
+  html += "<button class='btn btn-1 btn-1a' onclick='location.href=\"https://oomaelab.github.io/book.html\"'>研究で参考になる本</button>";
   html += "<button class='btn btn-1 btn-1a' onclick='location.href=\"https://oomaelab.github.io/learning.html\"'>授業資料など</button>";
   html += "<!--";
   html += "<button class='btn btn-1 btn-1a' onclick='location.href=\"https://oomaelab.github.io/schoolonly/lecture.html\"'>講義ノート(学内専用）</button>";
