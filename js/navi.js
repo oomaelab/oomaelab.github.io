65e069b2d5bb88f742e602506b6135c6fe683ca3
oomaelab <69372148+oomaelab@users.noreply.github.com>
2020-08-10 21:53:34 +0900

    Update navi.js

--- js/navi.js
+++ js/navi.js
@@ -15,12 +15,10 @@ function navigator()
   html += "</p>";
   html += "<p>";
   html += "<button class='btn btn-1 btn-1a' onclick='location.href=\"https://oomaelab.github.io/member.html\"'>メンバー</button>";
+  html += "<button class='btn btn-1 btn-1a' onclick='location.href=\"https://oomaelab.github.io/message.html\"'>研究室選び</button>";
   html += "<button class='btn btn-1 btn-1a' onclick='location.href=\"https://oomaelab.github.io/oomaelab/index.html\"'>研究室内部ページ</button>";
   html += "<button class='btn btn-1 btn-1a' onclick='location.href=\"https://oomaelab.github.io/book.html\"'>研究で参考になる本</button>";
   html += "<button class='btn btn-1 btn-1a' onclick='location.href=\"https://oomaelab.github.io/learning.html\"'>授業資料など</button>";
-  html += "<!--";
-  html += "<button class='btn btn-1 btn-1a' onclick='location.href=\"https://oomaelab.github.io/schoolonly/lecture.html\"'>講義ノート(学内専用）</button>";
-  html += "-->";
   html += "</p>";
   html += "</section>";
   html += "</div><!-- /container -->";
